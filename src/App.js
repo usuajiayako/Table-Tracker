11bdc13492c72b94101f8e305b457ee80e974aeb
lchapmb <lchapmb@googlemail.com>
2021-02-19 11:45:30 +0000

    refactors App to use switch for routes with amdin and waiter pages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
+
 import AdminPage from './containers/AdminPage/AdminPage';
 import CashierPage from './containers/CashierPage/CashierPage';
 import KitchenPage from './containers/KitchenPage/KitchenPage';
 import WaiterPage from './containers/WaiterPage/WaiterPage';
 
 const App = () => {
-  return (
-    <div className='App'>
-      <h1>Hello</h1>
-      <AdminPage />
-      <CashierPage />
-      <KitchenPage />
-      <WaiterPage />
-    </div>
+  const routes = (
+    <Switch>
+      <Route path='/admin' component={AdminPage} />
+      <Route path='/waiter' component={WaiterPage} />
+    </Switch>
   );
+
+  return <React.Fragment>{routes}</React.Fragment>;
 };
 
 export default App;
