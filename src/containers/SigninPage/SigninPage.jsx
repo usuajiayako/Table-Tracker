1b2c5222ff420df7ab187090550b240e3de04830
lchapmb <lchapmb@googlemail.com>
2021-02-22 09:59:52 +0000

    sets up sign up form using bootstrap

--- src/containers/SigninPage/SigninPage.jsx
+++ src/containers/SigninPage/SigninPage.jsx
@@ -1,9 +1,41 @@
-import "./SigninPage.css"
+import './SigninPage.css';
+import { useRef } from 'react';
+import { Form, Button, Card } from 'react-bootstrap';
 
 const SigninPage = () => {
-  return(
-    <h1>Signin Page</h1>
-  )
-}
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
+
+  return (
+    <div>
+      <Card>
+        <Card.Body>
+          <h2 className='text-center mb-4'>Sign Up</h2>
+        </Card.Body>
+        <Form>
+          <Form.Group id='email'>
+            <Form.Label>Email</Form.Label>
+            <Form.Control type='email' ref={emailRef} required />
+          </Form.Group>
+          <Form.Group id='password'>
+            <Form.Label>Password</Form.Label>
+            <Form.Control type='password' ref={passwordRef} required />
+          </Form.Group>
+          <Form.Group id='password-confirm'>
+            <Form.Label>Password Confirmation</Form.Label>
+            <Form.Control type='password' ref={passwordConfirmRef} required />
+          </Form.Group>
+        </Form>
+        <Button type='submit' className='w-100'>
+          Sign Up
+        </Button>
+      </Card>
+      <div className='w-100 text-center mt-2'>
+        Already have an account? Log In
+      </div>
+    </div>
+  );
+};
 
 export default SigninPage;
