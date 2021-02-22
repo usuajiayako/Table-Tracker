d20f0e3294e1ae74566e4db962f5f43d68f5cde1
lchapmb <lchapmb@googlemail.com>
2021-02-22 13:00:29 +0000

    removes unused import

--- src/containers/SigninPage/SigninPage.jsx
+++ src/containers/SigninPage/SigninPage.jsx
@@ -1,5 +1,4 @@
 import './SigninPage.css';
-import { useRef } from 'react';
 import { Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SignUp from './SignUp';
