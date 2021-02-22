f8038743235017570a189752d9a463bec74bdd5f
lchapmb <lchapmb@googlemail.com>
2021-02-22 16:00:17 +0000

    removes bootstrap

--- src/containers/SigninPage/SigninPage.jsx
+++ src/containers/SigninPage/SigninPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useRef, useState } from 'react';
 import { Form, Button, Card, Alert } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './SigninPage.scss';
 import { useAuth } from '../../context/AuthContext';
 import { useHistory } from 'react-router-dom';
 
@@ -29,21 +29,21 @@ function SigninPage() {
   }
 
   return (
-    <div className='w-100' style={{ maxWidth: '400px' }}>
+    <div className="login-container">
       <Card>
         <Card.Body>
-          <h2 className='text-center mb-4'>Log In</h2>
-          {error && <Alert variant='danger'>{error}</Alert>}
+          <h2 className="title">Log In</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
           <Form onSubmit={handleSubmit}>
-            <Form.Group id='email'>
+            <Form.Group id="email">
               <Form.Label>Email</Form.Label>
-              <Form.Control type='email' ref={emailRef} required />
+              <Form.Control type="email" ref={emailRef} required />
             </Form.Group>
-            <Form.Group id='password'>
+            <Form.Group id="password">
               <Form.Label>Password</Form.Label>
-              <Form.Control type='password' ref={passwordRef} required />
+              <Form.Control type="password" ref={passwordRef} required />
             </Form.Group>
-            <Button type='submit' className='w-100' disabled={loading}>
+            <Button type="submit" disabled={loading}>
               Log In
             </Button>
           </Form>
