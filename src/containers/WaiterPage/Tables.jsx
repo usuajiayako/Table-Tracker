9350f46d00288638f1b84d40de5c379b975aa830
lchapmb <lchapmb@googlemail.com>
2021-02-22 14:38:56 +0000

    creates tables component for waiter view

---
+++ src/containers/WaiterPage/Tables.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Tables() {
+  return <h2>Tables</h2>;
+}
+
+export default Tables;
--- src/containers/WaiterPage/WaiterPage.jsx
+++ src/containers/WaiterPage/WaiterPage.jsx
@@ -1,9 +1,25 @@
 import './WaiterPage.css';
-
+import { Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Tables from './Tables';
 
 const WaiterPage = () => {
-  return <div>Waiter Page</div>;
+  return (
+    <Container
+      className='d-flex align-items-center justify-content-center'
+      style={{ minHeight: '100vh' }}
+    >
+      <div className='w-100' style={{ maxWidth: '400px' }}>
+        <Router>
+          <Switch>
+            <Route exact path='/waiter' component={Tables} />
+          </Switch>
+        </Router>
+      </div>
+    </Container>
+  );
 };
 
 export default WaiterPage;
