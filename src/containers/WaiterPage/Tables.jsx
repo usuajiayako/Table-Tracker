a3196cfe926d650616e33a3456e6a201caa3d066
lchapmb <lchapmb@googlemail.com>
2021-02-22 14:46:40 +0000

    creates ul with elements displaying table names

--- src/containers/WaiterPage/Tables.jsx
+++ src/containers/WaiterPage/Tables.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { useState } from 'react';
 
 function Tables() {
-  return <h2>Tables</h2>;
+  const [tables] = useState([
+    { name: 'table1' },
+    { name: 'table2' },
+    { name: 'table3' }
+  ]);
+
+  return (
+    <>
+      <h2>Tables</h2>
+      <ul>
+        {tables.map((table) => {
+          return <li>{table.name}</li>;
+        })}
+      </ul>
+    </>
+  );
 }
 
 export default Tables;
