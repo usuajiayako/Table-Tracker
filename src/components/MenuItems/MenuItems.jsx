6f4932d1b3e2da065e83af6f524951d4bde10025
Melissa Astbury <melissaastbury@hotmail.com>
2021-02-23 12:43:05 +0000

    Styling for individual items

--- src/components/MenuItems/MenuItems.jsx
+++ src/components/MenuItems/MenuItems.jsx
@@ -2,33 +2,55 @@ import React, { useContext } from 'react';
 
 import { MenuContext } from '../../context/MenuContext';
 
+import './MenuItems.scss';
+
 const MenuItems = () => {
   const { starters, mains, desserts, drinks } = useContext(MenuContext);
 
   return (
     <>
-      <div>
+      <div className="course-container">
+        <h3 className="title">Starters</h3>
         {starters &&
           starters.map((starter) => {
-            return <p>{starter.name}</p>;
+            return (
+              <p key={starter.food_id} className="items">
+                {starter.name}
+              </p>
+            );
           })}
       </div>
-      <div>
+      <div className="course-container">
+        <h3 className="title">Mains</h3>
         {mains &&
           mains.map((main) => {
-            return <p>{main.name}</p>;
+            return (
+              <p key={main.food_id} className="items">
+                {main.name}
+              </p>
+            );
           })}
       </div>
-      <div>
+      <div className="course-container">
+        <h3 className="title">Desserts</h3>
         {desserts &&
           desserts.map((dessert) => {
-            return <p>{dessert.name}</p>;
+            return (
+              <p key={dessert.food_id} className="items">
+                {dessert.name}
+              </p>
+            );
           })}
       </div>
-      <div>
+      <div className="course-container">
+        <h3 className="title">Drinks</h3>
         {drinks &&
           drinks.map((drink) => {
-            return <p>{drink.name}</p>;
+            return (
+              <p key={drink.food_id} className="items">
+                {drink.name}
+              </p>
+            );
           })}
       </div>
     </>
