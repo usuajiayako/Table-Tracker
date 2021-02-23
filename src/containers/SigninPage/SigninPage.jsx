eac1b004a8e04bff381baaebefa0cf1c0b89f022
lchapmb <lchapmb@googlemail.com>
2021-02-23 16:01:30 +0000

    styles login

--- src/containers/SigninPage/SigninPage.jsx
+++ src/containers/SigninPage/SigninPage.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { useRef, useState } from 'react';
-import './SigninPage.scss';
-import { useAuth } from '../../context/AuthContext';
-import { useHistory } from 'react-router-dom';
+import React from "react";
+import { useRef, useState } from "react";
+import "./SigninPage.scss";
+import { useAuth } from "../../context/AuthContext";
+import { useHistory } from "react-router-dom";
 
 function SigninPage() {
   const emailRef = useRef();
   const passwordRef = useRef();
   const { login } = useAuth();
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
   const history = useHistory();
 
@@ -16,12 +16,12 @@ function SigninPage() {
     event.preventDefault();
 
     try {
-      setError('');
+      setError("");
       setLoading(true);
       await login(emailRef.current.value, passwordRef.current.value);
-      history.push('/admin');
+      history.push("/admin");
     } catch {
-      setError('Failed to sign in');
+      setError("Failed to sign in");
     }
 
     setLoading(false);
@@ -32,15 +32,15 @@ function SigninPage() {
       <h2 className="title">Log In</h2>
       {error && <p>{error}</p>}
       <form onSubmit={handleSubmit}>
-        <div class="form-group" id="email">
+        <div className="un " id="email">
           <label for="email">Email</label>
           <input type="email" ref={emailRef} required />
         </div>
-        <div class="form-group" id="password">
+        <div className="pass" id="password">
           <label for="password">Password</label>
           <input type="password" ref={passwordRef} required />
         </div>
-        <button type="submit" disabled={loading}>
+        <button className="submit" type="submit" disabled={loading}>
           Log In
         </button>
       </form>
