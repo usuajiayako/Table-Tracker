6b0f990607a3b9cfd6d495c8a9a9c259bb939f9c
lchapmb <lchapmb@googlemail.com>
2021-02-23 14:15:42 +0000

    moves tables component into waiter page

--- src/containers/WaiterPage/WaiterPage.jsx
+++ src/containers/WaiterPage/WaiterPage.jsx
@@ -1,12 +1,59 @@
 import "./WaiterPage.scss";
 import React from "react";
-import Tables from "./Tables";
+import { useState } from "react";
+
+import Popup from "../../components/Popup/Popup";
 
 const WaiterPage = () => {
+  const [tables] = useState([
+    { name: "table1" },
+    { name: "table2" },
+    { name: "table3" },
+  ]);
+  const [showPopup, setShowPopup] = useState(false);
+  const [menu] = useState([
+    { name: "pasta" },
+    { name: "salad" },
+    { name: "tacos" },
+  ]);
+  const [order, setOrder] = useState([]);
+
+  function togglePopup() {
+    setShowPopup(!showPopup);
+  }
+
+  function addToOrder(item) {
+    setOrder([item, ...order]);
+  }
+
+  function submitOrder(table, order) {
+    console.log(table, order);
+    setOrder([]);
+  }
   return (
     <>
       <h2>Waiter View</h2>
-      <Tables />
+      <ul>
+        {tables.map((table) => {
+          return (
+            <li key={table.name}>
+              <div class="table-icon" id={table.name} onClick={togglePopup}>
+                {table.name}
+              </div>
+              {showPopup ? (
+                <Popup
+                  table={table.name}
+                  menu={menu}
+                  order={order}
+                  closePopup={togglePopup.bind(this)}
+                  addToOrder={addToOrder.bind(this)}
+                  submitOrder={submitOrder.bind(this)}
+                />
+              ) : null}
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 };
