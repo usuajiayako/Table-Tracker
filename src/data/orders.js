15ac30ff001ca1cf7a55c72548f7234448a01585
lchapmb <lchapmb@googlemail.com>
2021-02-24 16:56:46 +0000

    adds food_items to food data

--- src/data/orders.js
+++ src/data/orders.js
@@ -9,5 +9,10 @@ export const orderData = [
     drinks_ready: "", // boolean
     is_active: "", // boolean
     created_at: "", // boolean
+    food_items: [
+      { name: "", course: "" },
+      { name: "", course: "" },
+      { name: "", course: "" },
+    ],
   },
 ];
