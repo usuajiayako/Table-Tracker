f6eade91b050d5c37cbf88183014ef1f51cbfa0f
lchapmb <lchapmb@googlemail.com>
2021-02-24 11:14:56 +0000

    adds conditional styling for tables, popup now has buttons to update status of tables

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -2,15 +2,21 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 import "./popup.scss";
 
-function Popup({ tableName, closePopup }) {
+function Popup({ table, closePopup, updateTableStatus }) {
   const history = useHistory();
   return (
     <div className="popup">
       <div className="popup_inner">
-        <h1>{tableName}</h1>
+        <h1>{table.name}</h1>
+        <h2>{table.status}</h2>
         <button onClick={closePopup}>Close Popup</button>
-        <button>Activate table</button>
-        <button onClick={() => history.push(`/menu?${tableName}`)}>
+        <button onClick={() => updateTableStatus(table, "active")}>
+          Activate table
+        </button>
+        <button onClick={() => updateTableStatus(table, "default")}>
+          De-activate table
+        </button>
+        <button onClick={() => history.push(`/menu?${table.name}`)}>
           Place order
         </button>
       </div>
