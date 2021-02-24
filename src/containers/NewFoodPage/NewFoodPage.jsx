ba68ce6905c98b16d571ef023609710a86b26154
Melissa Astbury <melissaastbury@hotmail.com>
2021-02-24 10:01:24 +0000

    started the structure for the food form

--- src/containers/NewFoodPage/NewFoodPage.jsx
+++ src/containers/NewFoodPage/NewFoodPage.jsx
@@ -1,7 +1,45 @@
+import React, { useState, useContext } from 'react';
 import './NewFoodPage.scss';
 
 const NewFoodPage = () => {
-  return <h1>This will be the page to add a new food</h1>;
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+  const [course, setCourse] = useState('');
+
+  const submitForm = () => {};
+
+  return (
+    <form onSubmit={submitForm} className="food-form">
+      <label htmlFor="name">
+        Food Name
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+      <label htmlFor="price">
+        Price
+        <input
+          type="text"
+          name="price"
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+        />
+      </label>
+      <label htmlFor="course">
+        Course
+        <input
+          type="text"
+          name="course"
+          value={course}
+          onChange={(e) => setCourse(e.target.value)}
+        />
+      </label>
+      <button type="submit">Add Food Item</button>
+    </form>
+  );
 };
 
 export default NewFoodPage;
