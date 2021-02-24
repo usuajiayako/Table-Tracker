1896fe6bd9edce5e389939dac7f9cc38f2e5af55
lchapmb <lchapmb@googlemail.com>
2021-02-24 16:37:52 +0000

    adds dummy order data file

---
+++ src/data/orders.js
@@ -0,0 +1,13 @@
+export const orderData = [
+  {
+    order_id: "", // int
+    table_id: "", // int
+    description: "", // string
+    starters_ready: "", // boolean
+    mains_ready: "", // boolean
+    desserts_ready: "", // boolean
+    drinks_ready: "", // boolean
+    is_active: "", // boolean
+    created_at: "", // boolean
+  },
+];
