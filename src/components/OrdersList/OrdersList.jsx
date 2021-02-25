7c9cb1f4eac2c0228143bb9c6b890ed38abaedfb
lchapmb <lchapmb@googlemail.com>
2021-02-25 10:13:43 +0000

    applies flex, formats date object to min, hours, seconds, now displays orders in list

--- src/components/OrdersList/OrdersList.jsx
+++ src/components/OrdersList/OrdersList.jsx
@@ -1,20 +1,28 @@
 import React, { useContext } from 'react';
+import './OrdersList.scss';
 
 import { OrderContext } from '../../context/OrderContext';
 
 function OrdersList() {
   const { orders } = useContext(OrderContext);
 
+  const dateOptions = {
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true,
+  };
+
   return (
     <>
       <h2>Active Orders</h2>
-      <ul>
+      <ul className="orders_list">
         {orders.map((order) => {
           return (
-            <li key={order.order_id}>
-              <p>Table: {order.table_id}</p>
+            <li key={order.order_id} className="order_card">
+              <strong>Table: {order.table_id}</strong>
               <p>Notes: {order.description}</p>
-              <div className="foodList">
+              <div className="food_list">
+                <strong>Order:</strong>
                 {order.food_items.map((item) => {
                   return (
                     <>
@@ -24,7 +32,12 @@ function OrdersList() {
                   );
                 })}
               </div>
-              <p>Order time: {order.create_at}</p>
+              <p>
+                Order time:{' '}
+                {new Intl.DateTimeFormat('en-GB', dateOptions).format(
+                  order.created_at
+                )}
+              </p>
               <button>Ready to serve</button>
             </li>
           );
