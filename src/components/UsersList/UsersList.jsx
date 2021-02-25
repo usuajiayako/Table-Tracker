15b1c5ec1d55af093b852af14e81b8f0bb08a0de
lchapmb <lchapmb@googlemail.com>
2021-02-25 11:47:22 +0000

    creates users data and imports to context, sets up list in UsersList

--- src/components/UsersList/UsersList.jsx
+++ src/components/UsersList/UsersList.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import { StaffContext } from '../../context/StaffContext';
 
 function UsersList() {
-  return <h2>Users List</h2>;
+  const { users } = useContext(StaffContext);
+  console.log(users);
+
+  return (
+    <>
+      <h2>Users List</h2>
+      <ul>
+        {users ? (
+          users.map((user) => {
+            return (
+              <li key={user.email}>
+                <h2>
+                  {user.first_name} {user.last_name}
+                </h2>
+                <p>Email: {user.email}</p>
+                <p>Staff role: {user.role}</p>
+              </li>
+            );
+          })
+        ) : (
+          <p>Loading users...</p>
+        )}
+      </ul>
+    </>
+  );
 }
 
 export default UsersList;
--- src/containers/AllUsersPage/AllUsersPage.jsx
+++ src/containers/AllUsersPage/AllUsersPage.jsx
@@ -8,7 +8,6 @@ import React from 'react';
 const AdminPage = () => {
   return (
     <StaffContextProvider>
-      <h1>This will be the staff profile list - default for admin</h1>;
       <UsersList />
     </StaffContextProvider>
   );
