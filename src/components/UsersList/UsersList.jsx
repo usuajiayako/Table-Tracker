1bd26d73fcbf2bb0b20814548913cb4391677585
lchapmb <lchapmb@googlemail.com>
2021-02-25 11:29:37 +0000

    sets up UsersList and nests in staff context provider

---
+++ src/components/UsersList/UsersList.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function UsersList() {
+  return <h2>Users List</h2>;
+}
+
+export default UsersList;
--- src/containers/AllUsersPage/AllUsersPage.jsx
+++ src/containers/AllUsersPage/AllUsersPage.jsx
@@ -1,9 +1,17 @@
 import './AllUsersPage.scss';
+import { StaffContextProvider } from '../../context/StaffContext';
+
+import UsersList from '../../components/UsersList/UsersList';
 
 import React from 'react';
 
 const AdminPage = () => {
-  return <h1>This will be the staff profile list - default for admin</h1>;
+  return (
+    <StaffContextProvider>
+      <h1>This will be the staff profile list - default for admin</h1>;
+      <UsersList />
+    </StaffContextProvider>
+  );
 };
 
 export default AdminPage;
