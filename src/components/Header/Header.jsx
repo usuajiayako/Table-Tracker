30e0084c000bd41ca8441720191801907d3ffcc7
lchapmb <lchapmb@googlemail.com>
2021-02-26 11:57:48 +0000

    creates function totoggle class on nav and hide/unhide navlinks

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,13 +2,34 @@ import React, { useContext } from 'react';
 import NavItem from './NavItem/NavItem';
 import './Header.scss';
 import { AuthContext } from '../../context/AuthContext';
+import { FaBars } from 'react-icons/fa';
 
 const Header = () => {
   const { currentUser } = useContext(AuthContext);
+
+  // function myFunction() {
+  //   var x = document.getElementById("myTopnav");
+  //   if (x.className === "topnav") {
+  //     x.className += " responsive";
+  //   } else {
+  //     x.className = "topnav";
+  //   }
+  // }
+
+  const burgerDropDown = () => {
+    const element = document.getElementsByTagName('ul');
+    if (element[0].className === 'nav-list') {
+      element[0].className += ' drop-down';
+    } else {
+      element[0].className = 'nav-list';
+    }
+  };
+
   return (
     <header>
       <h1 className="table-title">Table Tracker</h1>
       <nav>
+        <FaBars size={20} onClick={burgerDropDown} className="icon" />
         <ul className="nav-list">
           {!currentUser ? (
             <NavItem link="/">Sign In</NavItem>
@@ -32,15 +53,16 @@ const Header = () => {
               <NavItem link="/" signout={true} className="nav-item">
                 Sign Out
               </NavItem>
-              <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-                &#9776;
-              </a>
             </>
           ) : (
             <>
-              <NavItem link="/waiter">Tables</NavItem>
-              <NavItem link="/kitchen">Kitchen</NavItem>
-              <NavItem link="/" signout={true}>
+              <NavItem link="/waiter" className="nav-item">
+                Tables
+              </NavItem>
+              <NavItem link="/kitchen" className="nav-item">
+                Kitchen
+              </NavItem>
+              <NavItem link="/" signout={true} className="nav-item">
                 Sign Out
               </NavItem>
             </>
