640edea9fc6611e5ae9e6965548f0ed63ea5ef6e
lchapmb <lchapmb@googlemail.com>
2021-03-01 16:10:01 +0000

    adds post functionality for orders

--- src/context/OrderContext.js
+++ src/context/OrderContext.js
@@ -1,10 +1,30 @@
 import React, { createContext, useState, useEffect } from 'react';
 import { orderData as ordersArr } from '../data/orders';
+import axios from 'axios';
 
 export const OrderContext = createContext();
 
 const sendOrder = (finalisedOrder) => {
-  console.log(finalisedOrder);
+  const tableId = finalisedOrder.tableId;
+  const foodIds = finalisedOrder.order.map((order) => {
+    return order.food_item_id;
+  });
+  const description = '';
+  const foodData = {
+    food_items: foodIds,
+    description: description,
+  };
+  (async () => {
+    try {
+      await axios.post(
+        `http://localhost:9090/api/tables/${tableId}/orders`,
+        foodData
+      );
+      console.log('Order sent');
+    } catch (error) {
+      console.log(error.message, 'Sending order failed');
+    }
+  })();
 };
 
 const OrderContextProvider = (props) => {
