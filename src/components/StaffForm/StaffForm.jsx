04d546a0fd47ef706ccc63ae7614db8001840381
lchapmb <lchapmb@googlemail.com>
2021-03-02 17:03:30 +0000

    removes conflicts on button styling from popup,

--- src/components/StaffForm/StaffForm.jsx
+++ src/components/StaffForm/StaffForm.jsx
@@ -13,14 +13,14 @@ function StaffForm() {
   const [password, setPassword] = useState('');
   const [staffRole, setStaffRole] = useState('Staff');
 
-  const submitStaffForm = event => {
+  const submitStaffForm = (event) => {
     event.preventDefault();
     const staffInfo = {
       first_name: firstName,
       last_name: surname,
       email: email,
       password: password,
-      role: staffRole
+      role: staffRole,
     };
 
     addUser(staffInfo);
@@ -28,58 +28,61 @@ function StaffForm() {
   };
 
   return (
-    <div className="form-wrapper">
-      <form onSubmit={submitStaffForm} className="staff-form">
-        <label className="un">
-          First name
-          <input
-            type="text"
-            name="firstName"
-            value={firstName}
-            onChange={event => setFirstName(event.target.value)}
-          />
-        </label>
-        <label className="un">
-          Surname
-          <input
-            type="text"
-            name="surname"
-            value={surname}
-            onChange={event => setSurname(event.target.value)}
-          />
-        </label>
-        <label className="un">
-          Email address
-          <input
-            type="email"
-            name="email"
-            value={email}
-            onChange={event => setEmail(event.target.value)}
-          />
-        </label>
-        <label className="pass">
-          Password
-          <input
-            type="password"
-            name="password"
-            value={password}
-            onChange={event => setPassword(event.target.value)}
-          />
-        </label>
-        <label className="un">
-          Staff role
-          <select
-            name="staffRole"
-            value={staffRole}
-            onChange={event => setStaffRole(event.target.value)}
-          >
-            <option>Admin</option>
-            <option>Staff</option>
-          </select>
-        </label>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+    <form onSubmit={submitStaffForm} className="staff-form">
+      <label className="un">
+        First name
+        <br />
+        <input
+          type="text"
+          name="firstName"
+          value={firstName}
+          onChange={(event) => setFirstName(event.target.value)}
+        />
+      </label>
+      <label className="un">
+        Surname
+        <br />
+        <input
+          type="text"
+          name="surname"
+          value={surname}
+          onChange={(event) => setSurname(event.target.value)}
+        />
+      </label>
+      <label className="un">
+        Email address
+        <br />
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+      </label>
+      <label className="pass">
+        Password
+        <br />
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </label>
+      <label className="un">
+        Staff role
+        <br />
+        <select
+          name="staffRole"
+          value={staffRole}
+          onChange={(event) => setStaffRole(event.target.value)}
+        >
+          <option>Admin</option>
+          <option>Staff</option>
+        </select>
+      </label>
+      <button type="submit">Submit</button>
+    </form>
   );
 }
 
