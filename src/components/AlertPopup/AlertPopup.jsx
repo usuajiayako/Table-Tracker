b8bea530434f6bbbbc0f428ffcdf42170d5bae90
Ayako <usuajiayako@gmail.com>
2021-03-03 11:21:53 +0000

    Alert popup done

--- src/components/AlertPopup/AlertPopup.jsx
+++ src/components/AlertPopup/AlertPopup.jsx
@@ -1,20 +1,11 @@
 import React, { useContext } from 'react';
 import './AlertPopup.scss';
 
-// import { TableContext } from '../../context/TableContext';
-
 function AlertPopup({ table }) {
-  // const { updateTableStatus } = useContext(TableContext);
-
-  // console.log(table);
-
-  // const closeAlert = () => {
-  //   updateTableStatus(table.table_id, 'default');
-  // };
 
   return (
     <div className="alert-popup">
-      <h3>Order is {table.status}!</h3>
+      <h3>Food ready!</h3>
     </div>
   );
 }
