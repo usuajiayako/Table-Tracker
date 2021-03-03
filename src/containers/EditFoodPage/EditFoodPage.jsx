b089e6db9fa0db10286dc793b0e185a9941bea86
Ayako <usuajiayako@gmail.com>
2021-03-03 14:19:49 +0000

    added editfood page added link on navbar

---
+++ src/containers/EditFoodPage/EditFoodPage.jsx
@@ -0,0 +1,11 @@
+// import FoodForm from '../../components/FoodForm/FoodForm';
+import MenuContextProvider from '../../context/MenuContext';
+import './EditFoodPage.scss';
+
+const EditFoodPage = () => (
+  <MenuContextProvider>
+    <h1>Edit food</h1>
+  </MenuContextProvider>
+);
+
+export default EditFoodPage;
